2ba820caf862c4f1fd9d00767a4003fcb10ca490
mostafakarim1 <mostafa.karim@live.fr>
2019-06-27 15:29:45 +0200

    Updates

---
+++ precache-manifest.65d4f3828adfa534c0e2520e967ae689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821297fb0a3776c0bbe16318207b46e5",
+    "url": "/myhome-trust-killer/index.html"
+  },
+  {
+    "revision": "7fbb1852a5e87ec6f1af",
+    "url": "/myhome-trust-killer/static/css/main.92f7161a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/myhome-trust-killer/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7fbb1852a5e87ec6f1af",
+    "url": "/myhome-trust-killer/static/js/main.93c1d138.chunk.js"
+  },
+  {
+    "revision": "f9a19f883bc7f8d50f6f",
+    "url": "/myhome-trust-killer/static/js/runtime~main.8f4b99d2.js"
+  }
+]);
